6f4223ace599f5fef6bb7c2c1d2cbfe1ab2138f8
compscibaddie <143543274+compscibaddie@users.noreply.github.com>
2024-11-04 10:35:35 +0200

    Add files via upload

---
+++ DataSets/DataSets/DataHandler.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace DataSets
+{
+    class DataHandler
+    {
+        public DataHandler() { }
+        //set connection string
+        string connection = "Server = COMPSCI_BADDIE\\PINKDATABASE; Initial Catalog = BelgiumCampus; Integrated Security = SSPI";
+
+        public DataSet displayStudents()
+        {
+            //set sql query to run
+            string query = @"SELECT * FROM Students";
+            //vreate sqlDataAdapter object, and pass it query and connection string
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            //create data set object
+            DataSet dataSet = new DataSet();
+            //fill dataset object swith data fetched by sqldataAdapter
+            adapter.Fill(dataSet);
+            SaveDataSetToFile(dataSet, @"C:\Users\kekan\Desktop\PRG 282\DataSets\Students.txt");
+            return dataSet;
+            //store the data on a text file
+            
+
+        }
+        private void SaveDataSetToFile(DataSet dataSet, string filePath)
+        {
+            // Check if the dataset has tables and rows
+            if (dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
+            {
+                
+                // Open a StreamWriter to write data to a file
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    DataTable table = dataSet.Tables[0];
+
+                    // Write the column headers
+                    foreach (DataColumn column in table.Columns)
+                    {
+                        writer.Write(column.ColumnName + "\t");
+                    }
+                    writer.WriteLine();
+
+                    // Write each row's data
+                    foreach (DataRow row in table.Rows)
+                    {
+                        foreach (var item in row.ItemArray)
+                        {
+                            writer.Write(item.ToString() + "\t");
+                        }
+                        writer.WriteLine();
+                    }
+                }
+            }
+        }
+        public DataTable displayLectures()
+        {
+            //set sql query to run
+            string query = @"SELECT * FROM Lecturers";
+            //vreate sqlDataAdapter object, and pass it query and connection string
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            //create data set object
+            DataTable dataTable = new DataTable();
+            //fill dataset object swith data fetched by sqldataAdapter
+            adapter.Fill(dataTable);
+            SaveDataTableToFile(dataTable, @"C:\Users\kekan\Desktop\PRG 282\DataSets\Lecturers.txt");//calling the save method
+            return dataTable;
+            //store on a text file
+        }
+        private void SaveDataTableToFile(DataTable dataTable, string filePath)
+        {
+            // Check if the DataTable has rows
+            if (dataTable.Rows.Count > 0)
+            {
+                // Open a StreamWriter to write data to a file
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    // Write the column headers
+                    foreach (DataColumn column in dataTable.Columns)
+                    {
+                        writer.Write(column.ColumnName + "\t");
+                    }
+                    writer.WriteLine();
+
+                    // Write each row's data
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        foreach (var item in row.ItemArray)
+                        {
+                            writer.Write(item.ToString() + "\t");
+                        }
+                        writer.WriteLine();
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ DataSets/DataSets/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSets
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataHandler handler = new DataHandler();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = handler.displayStudents().Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = handler.displayLectures();
+        }
+    }
+}
